cc4001cf3f99e53530000b68cbbff8138c6231d1
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2018-03-30 16:00:13 +0300

    30/03 16.00

---
+++ js/autohouse.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+
+	$('section.slider .slider_wrap').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:1,
+		slidesToScroll:1,
+		autoplay:true,
+		loop:true,
+		pauseOnFocus:false,
+		pauseOnHover:false
+	});
+
+	$('.popular__slider-wrap').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:4,
+		slidesToScroll:1,
+		autoplay:false,
+		loop:true,
+		pauseOnFocus:false,
+		pauseOnHover:false,
+		responsive: [
+	    {
+	      breakpoint: 1001,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 1
+	      }
+	    }, 
+	    {
+	      breakpoint:701,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1
+	      }
+	    }, 
+	    {
+	      breakpoint:481,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	    ]
+	});
+
+	var mh = 0;
+    $(".tuningHouse__wrap").each(function () {
+       var h_block = parseInt($(this).height());
+       if(h_block > mh) {
+          mh = h_block;
+       };
+   });
+   $(".tuningHouse__wrap").height(mh);
+
+});
